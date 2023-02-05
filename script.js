65e21a7d35105187e49c59a3d94bf5b291a41edb
pooja062000 <vikaskosli9890@gmail.com>
2023-02-05 12:00:52 +0530

    update

--- script.js
+++ script.js
@@ -28,9 +28,25 @@ function weatherData(cityName){
         document.querySelector('.tem').innerHTML = `${Math.floor(cityData.main.temp - 273.15)}°c`
         document.querySelector('.weat').innerHTML = `${cityData.weather[0].main}`
         document.querySelector('.min-max').innerHTML = `Min ${Math.floor(cityData.main.temp_min - 273.15)}°c / Max ${Math.floor(cityData.main.temp_max - 273.15)}°c`
-        
-        
+       
+        let weaterStatus = `${cityData.weather[0].main}`
+        // console.log('status', weaterStatus)
+
+        document.querySelector('body').className = ''
+
+        if(weaterStatus === 'Mist'){
+            document.querySelector('body').classList.add('mist')
+        }else if(weaterStatus === 'Clear'){
+            document.querySelector('body').classList.add('clear')
+        }else if(weaterStatus === 'Clouds'){
+            document.querySelector('body').classList.add('clouds')
+        }else{
+            document.querySelector('body').className = ''
+        }
+
     })
+        
+    
     .catch(err => alert('City not found')) 
 }
 
